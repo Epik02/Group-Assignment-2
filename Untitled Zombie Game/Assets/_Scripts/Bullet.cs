ddd13817186897364a23cae9e6291ce931a18c44
Epik02 <72827304+Epik02@users.noreply.github.com>
2022-10-18 17:02:53 -0400

    Base

---
+++ Untitled Zombie Game/Assets/_Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        Destroy(gameObject);
+
+        if(other.collider.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        } else if(other.collider.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<EnemyController>().TakeDamages(25);
+            //Destroy(other.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        Destroy(gameObject, 3);
+    }
+}
